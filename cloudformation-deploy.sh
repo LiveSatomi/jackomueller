f184fe096d7febd1695b3c2ec73dff0475ee4d66
Jack Mueller <mueller.o.jack@gmail.com>
2021-12-31 18:40:43 -0500

    Remove extra log statement.

--- cloudformation-deploy.sh
+++ cloudformation-deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo $(which aws)
 npm run convertTemplate || exit 1
 aws cloudformation deploy --template-file template.json --stack-name jackomueller-stack  --capabilities CAPABILITY_IAM --parameter-overrides GithubRepo=LiveSatomi/jackomueller GithubBranch=master ProjectName=jackomueller
